cc838efafa6ca5ae0726265e38c325780d97dca5
JongSoo Lee <dldnxks12@naver.com>
2021-09-20 02:18:28 +0900

    Update and rename day1.py to with_cnn.py

--- with_cnn.py
+++ with_cnn.py
@@ -9,6 +9,7 @@ import torch.nn.functional as F
 
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
+from torch.utils.data import TensorDataset
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -22,29 +23,24 @@ print("Device", device)
 train_data = pd.read_csv("/content/drive/MyDrive/Colab Notebooks/Hackerton/Dacon/train_features.csv")
 train_label = pd.read_csv("/content/drive/MyDrive/Colab Notebooks/Hackerton/Dacon/train_labels.csv")
 
-out_list1 = []
+# sensor data to tensor
+out_list = []
 for i in range(3125):
   id = train_data.loc[(train_data['id'] == i)].values[:,2:]
-  out_list1.append(id)
+  out_list.append(id)
+  
+out_list = np.expand_dims(np.array(out_list), axis = 1)
+data = torch.from_numpy(out_list)  
 
-out_list1 = np.array(out_list1)
-out_list1 = np.expand_dims(out_list1, axis = 1)
-data = torch.from_numpy(out_list1)
-
+# label data to tensor
 out_list = []
 for i in range(3125):
   label = train_label.loc[(train_label['id'] == i)].values[0,1] 
-  if label == 26:
-    label = 0
-  else:
-    label = 1
-    
   out_list.append(label)
 
 out_list = np.array(out_list)
-out_list = out_list[:, np.newaxis]
-out_list = torch.Tensor(out_list)  
-label = out_list
+#out_list = np.expand_dims(np.array(out_list), axis = 1)
+label = torch.from_numpy(out_list)  
 
 class VGG_ORG(nn.Module):
     def __init__(self, in_channel): 
@@ -60,7 +56,7 @@ class VGG_ORG(nn.Module):
         nn.MaxPool2d(kernel_size = 2, stride = 2)            
         )
 
-        self.layer2= nn.Sequential(            # 32 x 300 x 3
+        self.layer2 = nn.Sequential(            # 32 x 300 x 3
         nn.Conv2d(32, 64, kernel_size = 3, stride = 1, padding = 1),    
         nn.BatchNorm2d(64),
         nn.ReLU(),
@@ -70,27 +66,29 @@ class VGG_ORG(nn.Module):
         nn.MaxPool2d(kernel_size = 3, stride = 1)            
         )
                         
-        self.fc1 = nn.Linear(64*298, 100), # 512 자리에 512 x width x heigth 넣어주기 
+        self.fc1 = nn.Linear(64*298, 100) # 512 자리에 512 x width x heigth 넣어주기 
         self.fc2 = nn.Linear(100, 61)
+
+        #self.last = nn.Softmax()
 
     def forward(self, x):
         
-        print("input shape", x.shape)
         # Conv layer
-        out = self.layer1(x)
-        print("out shape", out.shape) # out shape torch.Size([100, 32, 300, 3])
+        out = self.layer1(x)           # out shape torch.Size([100, 32, 300, 3])
+        out = self.layer2(out)         # out shape torch.Size([100, 64, 298, 1]) 
 
-        out = self.layer2(out)        
-        print("out shape", out.shape) # out shape torch.Size([100, 64, 298, 1])
-        
+        # flatten      
+        out = out.view(-1, 64*298)
+
         # fc layer    
         out = self.fc1(out)
-        print("out shape", out.shape)
         out = self.fc2(out)
-        print("out shape", out.shape)
+
 
         return out
+
       
+# Create Model       
 model = VGG_ORG(in_channel=1)
 
 batch_size = 100
@@ -98,37 +96,31 @@ learning_rate = 0.01
 num_epochs = 10
 
 optimizer = optim.SGD(model.parameters(), lr = learning_rate)
-loss = nn.BCELoss()
+loss = nn.CrossEntropyLoss()
 
-from torch.utils.data import TensorDataset
-
+# Create Dataloader 
 train_dataset = TensorDataset(data, label)
 train_loader = DataLoader(dataset = train_dataset, batch_size = batch_size, shuffle = True, drop_last = True)
 
+# Train
 for epoch in range(num_epochs+1):
   avg_cost = 0 
-  train_acc = 0 
   batch_length = len(train_loader)
   for x, y, in train_loader:    
-  
-    x = x.float()
-    y = x.float()
+    
+    pred = model(x.float())   # 100 x 61        
 
-    pred = model(x)          
-    cost = loss(x, y)
+    cost = loss(pred, y)
     
     optimizer.zero_grad() 
     cost.backward()       
     optimizer.step()      
 
-    _, pred = pred.max(1)
-    num_correct = (pred == y).sum().item() 
+    corr = torch.argmax(pred)
+    num_correct = (corr == y).sum().item() 
     avg_cost += cost / batch_length     
     acc = num_correct/batch_length 
     
   print("Accuracy", acc)    
   print("Average Cost", avg_cost)
-
-
-   
-
+  
